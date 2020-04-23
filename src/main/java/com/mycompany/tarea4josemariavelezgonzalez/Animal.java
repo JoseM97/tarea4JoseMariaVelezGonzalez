8a365749d35cb74fa50a8dd85f3bdb839f987352
JoseM97 <josem.velez.gonzalez@gmail.com>
2020-04-23 17:25:34 +0200

    Añade atributos en clase modelo

---
+++ src/main/java/com/mycompany/tarea4josemariavelezgonzalez/Animal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tarea4josemariavelezgonzalez;
+
+/**
+ *
+ * @author José
+ */
+public class Animal {
+    
+    String nombre;
+    double tamaño;
+    String raza;
+}
